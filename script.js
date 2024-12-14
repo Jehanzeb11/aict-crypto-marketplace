0b1b3e06baebb3d2fa27845dd5602f8ea8c98e10
Jehanzeb Siddiqui <100095101+Jehanzeb11@users.noreply.github.com>
2024-12-14 11:51:38 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
 const topCreator = document.querySelector(".top-creators");
+const catData = document.querySelector(".cat-data");
 
 const creators = [
   {
     img: "./assets/creator-1.png",
-    name: "BlueWhale",
+    name: "Keepitreal",
     sales: "34.53 ETH",
   },
 
   {
     img: "./assets/creator-2.png",
-    name: "BlueWhale",
+    name: "DigiLab",
     sales: "34.53 ETH",
   },
 
   {
     img: "./assets/creator-3.png",
-    name: "BlueWhale",
+    name: "GravityOne",
     sales: "34.53 ETH",
   },
   {
     img: "./assets/creator-4.png",
-    name: "BlueWhale",
+    name: "Juanie",
     sales: "34.53 ETH",
   },
   {
@@ -30,42 +31,77 @@ const creators = [
   },
   {
     img: "./assets/creator-6.png",
-    name: "BlueWhale",
+    name: "mr fox",
     sales: "34.53 ETH",
   },
   {
     img: "./assets/creator-7.png",
-    name: "BlueWhale",
+    name: "Shroomie",
     sales: "34.53 ETH",
   },
   {
     img: "./assets/creator-8.png",
-    name: "BlueWhale",
+    name: "robotica",
     sales: "34.53 ETH",
   },
   {
     img: "./assets/creator-9.png",
-    name: "BlueWhale",
+    name: "RustyRobot",
     sales: "34.53 ETH",
   },
   {
     img: "./assets/creator-10.png",
-    name: "BlueWhale",
+    name: "animakid",
     sales: "34.53 ETH",
   },
   {
     img: "./assets/creator-11.png",
-    name: "BlueWhale",
+    name: "Dotgu",
     sales: "34.53 ETH",
   },
   {
     img: "./assets/creator-12.png",
-    name: "BlueWhale",
+    name: "Ghiblier",
     sales: "34.53 ETH",
   },
 ];
 
-creators.forEach((creator,index) => {
+const categoryData = [
+  {
+    image: "./assets/category-1.png",
+    name: "Art"
+  },
+  {
+    image: "./assets/category-2.png",
+    name: "Collectibles"
+  },
+  {
+    image: "./assets/categor-3.png",
+    name: "Music"
+  },
+  {
+    image: "./assets/categor-4.png",
+    name: "Photography"
+  },
+  {
+    image: "./assets/categor-5.png",
+    name: "Video"
+  },
+  {
+    image: "./assets/categor-6.png",
+    name: "Utility"
+  },
+  {
+    image: "./assets/categor-7.png",
+    name: "Sport"
+  },
+  {
+    image: "./assets/categor-8.png",
+    name: "Virtual Worlds"
+  },
+]
+
+creators.forEach((creator, index) => {
   const creatorDiv = document.createElement("div");
   creatorDiv.classList.add("creator");
   creatorDiv.innerHTML = `
@@ -77,4 +113,17 @@ creators.forEach((creator,index) => {
   </div>
   `;
   topCreator.appendChild(creatorDiv);
+});
+
+
+categoryData.forEach((data, index) => {
+  const creatorDiv = document.createElement("div");
+  creatorDiv.classList.add("cat");
+  creatorDiv.innerHTML = `
+<img src=${data.image} alt="category-1.png">
+<div class="about">
+  <h4>${data.name}</h4>
+</div>
+  `;
+  catData.appendChild(creatorDiv);
 });
